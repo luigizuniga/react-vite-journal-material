634e97bea9063365be75fb1ceab61242ecda814f
luigizuniga <luigialbertozuniga@gmail.com>
2023-05-08 18:33:21 -0400

    noteView and useForm to view note active not edited notes

--- src/journal/views/NoteView.jsx
+++ src/journal/views/NoteView.jsx
@@ -1,13 +1,33 @@
+import { useMemo, useEffect } from 'react';
 import { SaveOutlined } from '@mui/icons-material';
 import { Button, Grid, TextField, Typography } from '@mui/material';
+import { useDispatch, useSelector } from 'react-redux';
+import { useForm } from '../../hooks/useForm';
 import { ImageGallery } from '../components';
+import { setActiveNote } from '../../store/journal';
 
 
 export const NoteView = () => {
+  const dispatch = useDispatch();
+
+  const { active:note } = useSelector( state => state.journal );
+
+   const { body , title, date, onInputChange, formState } = useForm( note );
+
+   const dateString = useMemo( () =>{
+        const newDate = new Date( date );
+        return newDate.toUTCString();
+   },[date]);
+
+   useEffect(() => {
+        dispatch( setActiveNote(formState));
+   }, [formState])
+   
+
   return (
     <Grid container direction='row' justifyContent='space-between' alignItems='center' sx={{ mb: 1 }}>
         <Grid item>
-            <Typography fontSize={ 39 } fontWeight='light' >28 de agosto, 2023</Typography>
+            <Typography fontSize={ 39 } fontWeight='light' >{ dateString }</Typography>
         </Grid>
         <Grid item>
             <Button color="primary" sx={{ padding: 2 }}>
@@ -24,6 +44,7 @@ export const NoteView = () => {
                 placeholder="Ingrese un título"
                 label="Título"
                 sx={{ border: 'none', mb: 1 }}
+                value={ title }
             />
 
             <TextField 
@@ -33,11 +54,12 @@ export const NoteView = () => {
                 multiline
                 placeholder="¿Qué sucedió en el día de hoy?"
                 minRows={ 5 }
+                value={ body }
             />
         </Grid>
 
         {/* Image gallery */}
-        <ImageGallery/>
+        {/* <ImageGallery/> */}
 
 
     </Grid>
